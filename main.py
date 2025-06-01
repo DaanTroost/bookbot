87cd7fb6ba1ec334afcf64615db8ced7a9c65164
DaanTroost <d.q.troost@outlook.com>
2025-06-01 22:45:10 +0200

    print a report of char counts.

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ def get_book_text(filepath: str) -> str:
 def main():
     book_content = get_book_text("books/frankenstein.txt")
     num_words = stats.get_num_words(book_content)
-    print(f"{num_words} words found in the document")
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
     char_count = stats.get_char_count(book_content)
-    print(f"charcount: {char_count}")
+    charlist = stats.get_sorted_char_count(char_count)
+    for entry in charlist:
+        if entry["char"].isalpha():
+            print(f'{entry["char"]}: {entry["num"]}')
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -5,9 +5,19 @@ def get_char_count(content: str) -> dict[str, int]:
     retdict = {}
     for char in content:
         lowerchar = char.lower()
-        if lowerchar.isalnum:
-            if lowerchar not in retdict:
-                retdict[lowerchar] = 1
-            else:
-                retdict[lowerchar] += 1
+        if lowerchar not in retdict:
+            retdict[lowerchar] = 1
+        else:
+            retdict[lowerchar] += 1
     return retdict
+
+def get_sorted_char_count(chardict: dict[str, int]) -> list[dict[str, int]]:
+    retlist = []
+    for key,value in chardict.items():
+        newkey = {"char": key, "num": value}
+        retlist.append(newkey)
+    retlist.sort(key=sort_func, reverse=True)
+    return retlist
+
+def sort_func(e):
+    return e['num']
